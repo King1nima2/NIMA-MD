15588db5b8db0f511889e64179a32b65d1702dbc
King1nima2 <king1nima2@gmail.com>
2025-03-02 15:14:22 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'KING_NIMA',
+  author: 'Bot',
+  botName: "NIMA MD",
+  botOwner: 'KING NIMA', // Your name
+  ownerNumber: '94769268470', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a NIMA MD bot.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
